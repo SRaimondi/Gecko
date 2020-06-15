817547572f5d5d6244f3b48b0f27099e30d224ac
Simone Raimondi <simo@cleverit.ch>
2020-06-15 18:27:32 +0200

    Added glad, initial code to setup window

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,55 @@
-#include "geometry/vector.hpp"
+// clang-format off
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+// clang-format on
+
+#include "spdlog/spdlog.h"
+
+static void processInput(GLFWwindow *const window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
 
 int main() {
-  using namespace Geometry;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if defined(__APPLE__)
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+  // Create GLFW window
+  GLFWwindow *window{glfwCreateWindow(800, 600, "Gecko", nullptr, nullptr)};
+  if (window == nullptr) {
+    spdlog::error("Could not create GLFW window");
+    glfwTerminate();
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
+
+  // Initialize GLAD
+  if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+    spdlog::error("Failed to initialize GLAD");
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 1;
+  }
+
+  // Main render loop
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
 
   return 0;
 }
